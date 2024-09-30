800fbf6022d86fab435e0b42e81f1f7c3e27ab64
Charlie Steenhagen <csteen1005@gmail.com>
2024-09-30 11:24:23 -0500

    Updated rgb on heimdall

--- keyboards/aesir/heimdall/keymaps/valhalla/keymap.c
+++ keyboards/aesir/heimdall/keymaps/valhalla/keymap.c
@@ -20,6 +20,67 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
+static uint32_t key_timer;               // timer for last keyboard activity, use 32bit value and function to make longer idle time possible
+static void     refresh_rgb(void);       // refreshes the activity timer and RGB, invoke whenever any activity happens
+static void     check_rgb_timeout(void); // checks if enough time has passed for RGB to timeout
+bool            is_rgb_timeout = false;  // store if RGB has timed out or not in a boolean
+
+void refresh_rgb(void) {
+    key_timer = timer_read32(); // store time of last refresh
+    if (is_rgb_timeout) {
+        is_rgb_timeout = false;
+        rgb_matrix_enable_noeeprom();
+    }
+}
+
+void check_rgb_timeout(void) {
+    if (!is_rgb_timeout && timer_elapsed32(key_timer) > RGB_MATRIX_TIMEOUT) // check if RGB has already timeout and if enough time has passed
+    {
+        rgb_matrix_disable_noeeprom();
+        is_rgb_timeout = true;
+    }
+}
+/* Then, call the above functions from QMK's built in post processing functions like so */
+/* Runs at the end of each scan loop, check if RGB timeout has occured or not */
+void housekeeping_task_user(void) {
+#ifdef RGB_MATRIX_TIMEOUT
+    check_rgb_timeout();
+#endif
+}
+
+/* Runs after each encoder tick, check if activity occurred */
+void post_encoder_update_user(uint8_t index, bool clockwise) {
+#ifdef RGB_MATRIX_TIMEOUT
+    refresh_rgb();
+#endif
+}
+
+void suspend_power_down_user(void) {
+    // code will run multiple times while keyboard is suspended
+}
+
+void suspend_wakeup_init_user(void) {
+    // code will run on keyboard wakeup
+}
+
+// Use this to clear eeprom
+// void eeconfig_init_user(void) {
+//     eeconfig_update_rgb_matrix_default();
+//     rgb_matrix_enable_noeeprom();
+// }
+
+bool rgb_matrix_indicators_user(void) {
+    rgb_matrix_enable();
+    rgb_matrix_set_color_all(RGB_BLUE);
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(8, RGB_BLUE);
+    } else {
+        rgb_matrix_set_color(8, RGB_BLACK);
+    }
+    return false;
+}
+
+
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
--- keyboards/aesir/heimdall/keymaps/valhalla/rules.mk
+++ keyboards/aesir/heimdall/keymaps/valhalla/rules.mk
@@ -1,3 +1,6 @@
 VIA_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 USER_NAME := csteamengine
+
+# RGB Matrix enabled
+RGB_MATRIX_ENABLE = yes
