b6ac48388299303a87795dd8bde8c97979c4b81d
Charlie Steenhagen <csteen1005@gmail.com>
2024-10-02 11:12:48 -0500

    Fixed pinout a bit

--- keyboards/aesir/heimdall/keymaps/valhalla/keymap.c
+++ keyboards/aesir/heimdall/keymaps/valhalla/keymap.c
@@ -11,12 +11,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
                  SELECT_WORD,   TO(_BASE),   TO(_FN0),   TO(_FN1),   TO(_FN2),   MO(_FN2),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
-        KC_NO,   KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13, KC_NO,    KC_DEL,      KC_END,      KC_HOME,    KC_NO,
-        KC_NO,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,         KC_PPLS,     KC_PMNS,     KC_PAST,    KC_PSLS,
+        QK_BOOT,   KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13, KC_DEL,    KC_NO,      KC_END,      KC_HOME,    KC_NUM,
+        QK_RBT,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,         KC_PPLS,     KC_PMNS,     KC_PAST,    KC_PSLS,
         KC_NO,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,         KC_KP_7,     KC_KP_8,     KC_KP_9,    KC_KP_DOT,
         KC_NO,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,          KC_KP_4,     KC_KP_5,     KC_KP_6,    KC_UP,
-        KC_NO,   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,         KC_KP_1,     KC_KP_2,     KC_KP_3,    KC_DOWN,
-        KC_NO,   KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT, KC_APP,    KC_RCTL,         KC_PENT,     KC_KP_0,     KC_LEFT,    KC_RIGHT
+        RGB_HUI,   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,         KC_KP_1,     KC_KP_2,     KC_KP_3,    KC_DOWN,
+        RGB_HUD,   KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT, KC_APP,    KC_RCTL,         KC_PENT,     KC_KP_0,     KC_LEFT,    KC_RIGHT
     )
 };
 // clang-format on
