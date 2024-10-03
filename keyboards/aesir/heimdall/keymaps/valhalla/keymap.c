5b9d9b9ddb5e9c89a0ca8fc2ecf022a16a6cb65d
Charlie Steenhagen <csteen1005@gmail.com>
2024-10-03 09:22:00 -0500

    Formatting

--- keyboards/aesir/heimdall/keymaps/valhalla/keymap.c
+++ keyboards/aesir/heimdall/keymaps/valhalla/keymap.c
@@ -54,12 +54,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_FN0] = LAYOUT(
                                 TD(TD_PW_ONE),      TD(TD_PW_TWO),      TD(TD_PW_THREE),    TD(TD_PW_FOUR),     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,      KC_NO,  TO(_FN0),
-        RGB_M_P,                  KC_ESC,             KC_F1,              KC_F2,              KC_F3,              KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,      KC_F12,     KC_F13, KC_NO,      KC_PPLS,    KC_END,      KC_HOME,    KC_NUM,
-        RGB_TOG,                  KC_GRV,             KC_1,               KC_2,               KC_3,               KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,     KC_BSPC,            KC_DEL,     KC_PMNS,     KC_PAST,     KC_PSLS,
-        RGB_MOD,            KC_TAB,             KC_Q,               KC_W,               KC_E,               KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,     KC_RBRC,    KC_BSLS,            KC_KP_DOT,  KC_KP_7,     KC_KP_8,     KC_KP_9,
-        RGB_RMOD,                KC_CAPS,            KC_A,               KC_S,               KC_D,               KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_ENT,                         KC_UP,      KC_KP_4,     KC_KP_5,     KC_KP_6,
-        RGB_HUI,        TD(TD_SC_LSHIFT),   KC_Z,               KC_X,               KC_C,               KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_SC_RSHIFT),                            KC_DOWN,    KC_KP_1,     KC_KP_2,     KC_KP_3,
-        RGB_HUD,       KC_LCTL,            KC_LALT,            KC_LGUI,                                KC_SPC,                             KC_RGUI, KC_RALT, KC_APP,  KC_RCTL,                                     KC_LEFT,    KC_RIGHT,    KC_KP_0,     KC_PENT
+        RGB_M_P,                KC_ESC,             KC_F1,              KC_F2,              KC_F3,              KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,      KC_F12,     KC_F13, KC_NO,      KC_PPLS,    KC_END,      KC_HOME,    KC_NUM,
+        RGB_TOG,                KC_GRV,             KC_1,               KC_2,               KC_3,               KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,     KC_BSPC,            KC_DEL,     KC_PMNS,     KC_PAST,     KC_PSLS,
+        RGB_MOD,                KC_TAB,             KC_Q,               KC_W,               KC_E,               KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,     KC_RBRC,    KC_BSLS,            KC_KP_DOT,  KC_KP_7,     KC_KP_8,     KC_KP_9,
+        RGB_RMOD,               KC_CAPS,            KC_A,               KC_S,               KC_D,               KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_ENT,                         KC_UP,      KC_KP_4,     KC_KP_5,     KC_KP_6,
+        RGB_HUI,                TD(TD_SC_LSHIFT),   KC_Z,               KC_X,               KC_C,               KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_SC_RSHIFT),                            KC_DOWN,    KC_KP_1,     KC_KP_2,     KC_KP_3,
+        RGB_HUD,                KC_LCTL,            KC_LALT,            KC_LGUI,                                KC_SPC,                             KC_RGUI, KC_RALT, KC_APP,  KC_RCTL,                                     KC_LEFT,    KC_RIGHT,    KC_KP_0,     KC_PENT
     ),
     [_FN1] = LAYOUT(
                                 TD(TD_PW_ONE),      TD(TD_PW_TWO),      TD(TD_PW_THREE),    TD(TD_PW_FOUR),     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,      KC_NO,  TO(_FN0),
